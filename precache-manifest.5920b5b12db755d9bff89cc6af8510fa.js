dffceb5fc367c6d0a55736648c81641fe12e3cb2
anand raj <rajanand9039@gmail.com>
2022-01-22 14:05:28 +0530

    Updates

---
+++ precache-manifest.5920b5b12db755d9bff89cc6af8510fa.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87a8a7d7ca2a36b87b9f978c54740e66",
+    "url": "/React-youtube/index.html"
+  },
+  {
+    "revision": "fcc4737f71fab49439ed",
+    "url": "/React-youtube/static/js/2.141b52df.chunk.js"
+  },
+  {
+    "revision": "0664289205b286537f7f",
+    "url": "/React-youtube/static/js/main.d6175f43.chunk.js"
+  },
+  {
+    "revision": "f4a725e1c0b7eab8121a",
+    "url": "/React-youtube/static/js/runtime~main.e7454856.js"
+  }
+]);
